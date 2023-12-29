3a9ab06f96385f91193cb87fffef3bf7c3e97a42
haeyong9701 <haeyong9701@gmail.com>
2023-12-30 01:47:24 +0900

    feat: fetch로 데이터 불러오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
+import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
 import { useState } from "react";
 
 const App = () => {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -16,11 +16,17 @@ const App = () => {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>베스트순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 };
