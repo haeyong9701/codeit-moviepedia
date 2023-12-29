024e76dd35450c61924d52a7603f0cb7a917cfd8
haeyong9701 <haeyong9701@gmail.com>
2023-12-30 02:30:40 +0900

    feat: 서버에서 정렬 데이터 가져오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -16,17 +16,20 @@ const App = () => {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
-    const { reviews } = await getReviews();
+  const handleLoad = async (orderQuery) => {
+    const { reviews } = await getReviews(orderQuery);
     setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad(order);
+  }, [order]);
 
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>베스트순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 };
